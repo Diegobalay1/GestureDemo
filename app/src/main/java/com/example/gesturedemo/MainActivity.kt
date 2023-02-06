8fb019a3e307e6748153af535a287ecf472d29b7
balay <dlolhd@gmail.com>
2023-02-06 09:35:30 +0100

    Commit initial. Detecting click gestures.

---
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.gesturedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gesturedemo.ui.theme.GestureDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GestureDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    ClickDemo()
+}
+
+@Composable
+fun ClickDemo() {
+    var colorState by remember { mutableStateOf(true) }
+    var bgColor by remember { mutableStateOf(Color.Blue) }
+
+    val clickHandler = {
+        colorState = !colorState
+        bgColor = if (colorState) {
+            Color.Blue
+        } else {
+            Color.DarkGray
+        }
+    }
+    Box(modifier = Modifier
+        .clickable { clickHandler() }
+        .background(bgColor)
+        .size(100.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GestureDemoTheme {
+        MainScreen()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
