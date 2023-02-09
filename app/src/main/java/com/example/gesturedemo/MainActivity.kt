b805b01addafff4edce93fd5230c810c241cf125
balay <dlolhd@gmail.com>
2023-02-09 12:13:18 +0100

    Detecting drag gestures using PointerInputScope.

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -44,7 +41,29 @@ class MainActivity : ComponentActivity() {
 fun MainScreen() {
     //ClickDemo()
     //TapPressDemo()
-    DragDemo()
+    //DragDemo()
+    PointerInputDrag()
+}
+
+@Composable
+fun PointerInputDrag() {
+    // TODO por hacer :)
+    Box(modifier = Modifier.fillMaxSize()) {
+        var xOffset by remember { mutableStateOf(0f) }
+        var yOffset by remember { mutableStateOf(0f) }
+
+        Box(modifier = Modifier
+            .offset { IntOffset(x = xOffset.roundToInt(), y = yOffset.roundToInt()) }
+            .background(Color.Blue)
+            .size(100.dp)
+            .pointerInput(Unit) {
+                detectDragGestures { _, distance ->
+                    xOffset += distance.x
+                    yOffset += distance.y
+                }
+            }
+        )
+    }
 }
 
 @Composable
